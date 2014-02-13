7cd89dc12d25f9f6d9c2e7f283129f03b7189c99
Philip Youssef <pyoussef@twitter.com>
2014-02-12 19:17:59 -0800

    Rows in GraderCardFragment now render a lot more nicely instead of relying on the mechanics of EditText.

--- Prism/src/main/java/com/phyous/prism/GraderCardFragment.java
+++ Prism/src/main/java/com/phyous/prism/GraderCardFragment.java
@@ -1,17 +1,19 @@
 package com.phyous.prism;
 
+import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.view.View.MeasureSpec;
+import android.widget.TextView;
 
+import com.phyous.prism.util.PositionalTextWatcher;
 import com.phyous.prism.widget.TypefacedTextView;
 
 import java.util.ArrayList;
@@ -20,16 +22,11 @@ public class GraderCardFragment extends Fragment {
     private String mTitle;
     private ArrayList<String> mInitialText;
     private int mColor;
+    private LinearLayout mLinearLayout;
 
     private static final String TITLE_INDEX = "title_index";
     private static final String INITIAL_TEXT_INDEX = "initial_text_index";
     private static final String COLOR_INDEX = "color_index";
-
-    public GraderCardFragment() {
-        mTitle = null;
-        mInitialText = null;
-        mColor = 1;
-    }
 
 
     public GraderCardFragment(String title, ArrayList<String> initialText, int color) {
@@ -47,19 +44,12 @@ public class GraderCardFragment extends Fragment {
             mColor = savedInstanceState.getInt(COLOR_INDEX);
         }
 
-        View rootView = inflater.inflate(R.layout.fragment_grader_card, container, false);
-        TypefacedTextView tv = (TypefacedTextView) rootView.findViewById(R.id.title);
+        final View rootView = inflater.inflate(R.layout.fragment_grader_card, container, false);
+        final TypefacedTextView tv = (TypefacedTextView) rootView.findViewById(R.id.title);
         tv.setText(mTitle);
 
-        ListView listView = (ListView) rootView.findViewById(R.id.text_entry_list);
-        // TODO: Abstract this a bit
-        mInitialText.add("");
-        String[] entryArray = new String[mInitialText.size()];
-        EntryArrayAdapter adapter = new EntryArrayAdapter(
-                getActivity(),
-                mInitialText.toArray(entryArray));
-        listView.setAdapter(adapter);
-        updateListviewHeight(listView);
+        mLinearLayout = (LinearLayout) rootView.findViewById(R.id.text_entry_list);
+        initializeTextEntryList();
 
         RelativeLayout innerBox = (RelativeLayout) rootView.findViewById(R.id.inner_box);
         GradientDrawable layoutBG = (GradientDrawable) innerBox.getBackground();
@@ -68,41 +58,94 @@ public class GraderCardFragment extends Fragment {
         return rootView;
     }
 
+    private void initializeTextEntryList() {
+        for (int i = 0; i < getAllCells(); i++) {
+            createListRow(i);
+        }
+    }
+
+    private void addTextEntryRow() {
+        createListRow(getFilledCells());
+    }
+
+    private void deleteTextEntryRow(View row) {
+        int cursorPos = mLinearLayout.indexOfChild(row) - 1;
+        mLinearLayout.removeView(row);
+
+        if(cursorPos >= 0) {
+            View rowView = mLinearLayout.getChildAt(cursorPos);
+            TextView textView = (TextView) rowView.findViewById(R.id.text_entry);
+            textView.requestFocus();
+        }
+    }
+
+    private void darkenFilledCell() {
+        int secondToLast = mLinearLayout.getChildCount() - 2;
+        View rowView = mLinearLayout.getChildAt(secondToLast);
+        ImageView image = (ImageView) rowView.findViewById(R.id.list_nub);
+        image.setImageResource(R.drawable.ic_drawer_dark);
+    }
+
+    /**
+     * Create a listrow for the linear layout of the fragment at a given position
+     * @param position where to create the row in the LinearLayout. If position >= getFilledCells()
+     *                 we will lighten the drawer icon to the left of the EditText field.
+     */
+    private void createListRow(int position) {
+        ViewGroup parent = mLinearLayout;
+        LayoutInflater inflater = (LayoutInflater) getActivity()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.grader_row_item, parent, false);
+
+        TextView textView = (TextView) rowView.findViewById(R.id.text_entry);
+        ImageView image = (ImageView) rowView.findViewById(R.id.list_nub);
+
+        if (position >= getFilledCells()) {
+            image.setImageResource(R.drawable.ic_drawer_light);
+            textView.addTextChangedListener(new PositionalTextWatcher(rowView) {
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if (s.length() > 0 && !getChangedState()) {
+                        setChangedState(true);
+                        addTextEntryRow();
+                        mInitialText.add(s.toString());
+                        darkenFilledCell();
+                    } else if (s.length() == 0 && getChangedState()) {
+                        deleteTextEntryRow(getParentRow());
+                    } else {
+                        mInitialText.set(mInitialText.size()-1, s.toString());
+                    }
+                }
+            });
+        } else {
+            String text = mInitialText.get(position);
+            textView.setText(text);
+            image.setImageResource(R.drawable.ic_drawer_dark);
+        }
+        parent.addView(rowView);
+    }
+
+    /**
+     * Get number of cells filled with text
+     * @return number of cells
+     */
+    private int getFilledCells() {
+        return mInitialText.size();
+    }
+
+    /**
+     * Get total number of cells filled with text and empty
+     * @return number of cells
+     */
+    private int getAllCells() {
+        return mInitialText.size() + 1;
+    }
+
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putString(TITLE_INDEX, mTitle);
         savedInstanceState.putStringArrayList(INITIAL_TEXT_INDEX, mInitialText);
         savedInstanceState.putInt(COLOR_INDEX, mColor);
-    }
-
-    private void updateListviewHeight(ListView listView) {
-        ListAdapter listAdapter = listView.getAdapter();
-        if (listAdapter == null) {
-            return;
-        }
-
-        int totalHeight = 0;
-        int desiredWidth = MeasureSpec.makeMeasureSpec(listView.getWidth(), MeasureSpec.AT_MOST);
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            View listItem = listAdapter.getView(i, null, listView);
-            listItem.measure(desiredWidth, MeasureSpec.UNSPECIFIED);
-            totalHeight += listItem.getMeasuredHeight();
-        }
-
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = totalHeight
-                + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
-        listView.setLayoutParams(params);
-        listView.requestLayout();
-    }
-
-    private String arrayToString(String[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for(String s: arr) {
-            sb.append(s);
-            sb.append("\n");
-        }
-        return sb.toString();
     }
 }
---
+++ Prism/src/main/java/com/phyous/prism/util/PositionalTextWatcher.java
@@ -0,0 +1,31 @@
+package com.phyous.prism.util;
+
+import android.text.TextWatcher;
+import android.view.View;
+
+public abstract class PositionalTextWatcher implements TextWatcher{
+    private View mRow;
+    private boolean mChanged = false;
+
+    public PositionalTextWatcher(View row) {
+        mRow = row;
+    }
+
+    public View getParentRow() {
+        return mRow;
+    }
+
+    public boolean getChangedState() {
+        return mChanged;
+    }
+
+    public void setChangedState(boolean changed) {
+        mChanged = changed;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) { }
+}
